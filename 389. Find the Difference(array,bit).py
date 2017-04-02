02c8c3606ea84ce5dc1d467afd491fdf8734469d
Cheng Chi <chixx078@umn.edu>
2017-04-01 20:50:10 -0500

    Create 389. Find the Difference(array,bit).py

---
+++ 389. Find the Difference(array,bit).py
@@ -0,0 +1,35 @@
+class Solution(object):
+    """
+    dictionary
+    """
+    def findTheDifference(self, s, t):
+        dic = {}
+        for ch in s:
+            dic[ch] = dic.get(ch, 0) + 1
+        for ch in t:
+            if dic.get(ch, 0) == 0:
+                return ch
+            else:
+                dic[ch] -= 1
+
+class Solution(object):
+    """
+    difference
+    """
+    def findTheDifference(self, s, t):
+        diff = 0
+        for i in range(len(s)):
+            diff -= ord(s[i])
+            diff += ord(t[i])
+        diff += ord(t[-1])
+        return chr(diff)
+
+class Solution(object):
+    """
+    xor
+    """
+    def findTheDifference(self, s, t):
+        code = 0
+        for ch in s + t:
+            code ^= ord(ch)
+        return chr(code)
